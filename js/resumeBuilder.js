f5dbabb85b16a84d14733a59d9d5dff5aed29527
Georgina Garcia-Obledo <georgina376@gmail.com>
2016-07-23 11:37:38 -0700

    add name and role to header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,9 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ var bio = {   "name" : "Georgina",   "role": "Web Developer" };
+ var formattedRole = HTMLheaderRole.replace("%data%", bio.role); 
+ $("#title").prepend(formattedRole); 
+ var formattedName = HTMLheaderName.replace("%data%", bio.name); 
+ $("#title").prepend(formattedName);
+console.log('hello');
