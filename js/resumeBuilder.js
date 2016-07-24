8e88602619eea0b94908510918d84cb0888659f3
Georgina Garcia-Obledo <georgina376@gmail.com>
2016-07-23 18:07:21 -0700

    add formatted name and role to header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,6 +1,12 @@
 // $("#main").append("Georgina Garcia-Obledo");
-var awesomeThoughts = "I am Georgina, and I am AWESOME!";
-console.log(awesomeThoughts);
-var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
-console.log(funThoughts);
-$("#main").append(funThoughts);
+//var awesomeThoughts = "I am Georgina, and I am AWESOME!";
+//console.log(awesomeThoughts);
+//var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
+//console.log(funThoughts);
+//$("#main").append(funThoughts);
+
+var formattedName = HTMLheaderName.replace("%data%", "Georgina Garcia-Obledo");
+$("#header").append(formattedName);
+
+var formattedRole = HTMLheaderRole.replace("%data%", "Product Manager");
+$("#header").append(formattedRole);
