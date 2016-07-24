3a0cc6edbb08ac4f52e4661e75cf40ecb3172416
Georgina Garcia-Obledo <georgina376@gmail.com>
2016-07-23 23:25:19 -0700

    try to add bio information but not working not sure why

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -7,9 +7,29 @@
 
 var name = "Georgina Garcia-Obledo";
 var role = "Product Manager";
+var email = "georgina376@gmail.com";
+var github = "georginagarciaobledo";
+//var location = ["San Francisco", "Los Angeles"];
+var skills = ["research", "planning"];
+//var pic = "images/headshot.jpg";
+//var welcome = "Welcome to my interactive resume."
 
 var formattedName = HTMLheaderName.replace("%data%", name);
 var formattedRole = HTMLheaderRole.replace("%data%", role);
+var formattedEmail = HTMLemail.replace("%data%", email);
+var formattedGithub = HTMLgithub.replace("%data%", github);
+//var formattedLocation = HTMLlocation.replace("%data%", location);
+var formattedSkills = HTMLskills.replace("%data", skills);
+//var formattedPic = HTMLbioPic.replace("%data%", pic);
+//var foramttedWelcomeMsg = HTMLwelcomeMsg.("%data%", welcome);
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+$("#main").append(formattedEmail);
+$("#main").append(formattedGithub);
+//$("#main").append(formattedLocation);
+$("#main").append(formattedSkills);
+
+
+
+
