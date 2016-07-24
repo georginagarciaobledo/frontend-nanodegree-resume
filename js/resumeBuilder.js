84fe2975f1e0d257e93bf383793887402c50dd74
Georgina Garcia-Obledo <georgina376@gmail.com>
2016-07-24 14:24:57 -0700

    add work object with array of jobs and education object with school and onlineCourses arrays. put in comments becuase something wrong, don't know what, but when not it in comments, makes everything on page disappear

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -36,68 +36,93 @@ $("#header").append(formattedBioPic);
 $("#header").append(formattedWelcomeMsg);
 $("#header").append(formattedSkills);
 
+/*
 var work = {};
 
 work.jobs = [];
 
 work.jobs[0] = {
-	"title": "Intern"
-
+	"employer": "Cocoon Cam",
+	"title": "Customer Success Intern",
+	"location": "San Francisco, CA",
+	"dates": "June 2016-Present",
+	"description": "This is what I do. Here is our website: www.cocooncam.com"
 };
 
-$("#main").append(work.jobs[0].title);
-
-/*
-work.j
-				{
-					"employer": "Cocoon Cam",
-					"title": "Customer Success Intern",
-					"location": "San Francisco, CA",
-					"dates": "June 2016-Present",
-					"description": "this is what I do. Here is our website: www.cocooncam.com"
-				},
-				{
-					"employer": "Office of Student Life, Occidental College"
-				},
-				{},
-				{},
-				{}
-			]
-	//display: function taking no parameters		
+work.jobs[1] = {
+	"employer": "Occidental College",
+	"title": "Managing Editor, La Encina Yearbook",
+	"location": "San Francisco, CA",
+	"dates": "May 2016-Present",
+	"description": "This is what I do. Here is our website: www.oxy.edu/la-encina"
 };
 
-
-var education = {
-	"schools": [{
-		"name": "Occidental College",
-		"location": "Los Angeles, CA",
-		"degree": "B.A., Expected May 21, 2017",
-		"majors": "Economics Major, Mathematics Minor",
-		"dates": "2014-2017",
-		"url": "www.oxy.edu"
-		},
-		{
-		"name": "City College of San Francisco",
-		"location": "San Francisco, CA",
-		"degree": "none",
-		"majors": "Coursework in German, Bookbinding",
-		"dates": "2013-2014",
-		"url": "www.ccsf.edu"
-		},
-		{
-		"name": "Harvard University",
-		"location": "Cambridge, MA",
-		"degree": "Harvard College Class of 2015",
-		"majors": "Coursework in . Working toward B.A. but transferred to Occidental College",
-		"dates": "2011-2012",
-		"url": "www.harvard.edu",
-		}],
-	"onlineCourses": [{
-		"title": "Front-End Web Developer Nanodegree, Expected August 2016",
-		"school": "Udacity",
-		"dates": "June 2016-August 2016",
-		"url": "www.udacity.com"
-	}]
-	//display: function taking no parameters
+work.jobs[2] = {
+	"employer": "Occidental College",
+	"title": "Economics Students Association",
+	"location": "San Francisco, CA",
+	"dates": "May 2016-Present",
+	"description": "This is what I do. Here is our website: ..."
 };
+
+work.jobs[3] = {
+	"employer": "Koru@Facebook",
+	"title": "Program Participant",
+	"location": "San Francisco, CA",
+	"dates": "July 2015",
+	"description": "This is what I do. Here is our website: joinkoru.com"
+};
+
+work.jobs[4] = {
+	"employer": "Cullinane Construction",
+	"title": "Admin Assistant",
+	"location": "San Francisco, CA",
+	"dates": "December 2013-Present",
+	"description": "This is what I do. I worked here full time December 2013-July 2014. Now part-time."
+};
+
+//work.display = function taking no parameters
+
+var education = {};
+
+education.schools = [];
+
+education.schools[0] = {
+	"name": "Occidental College",
+	"location": "Los Angeles, CA",
+	"degree": "B.A., Expected May 21, 2017",
+	"majors": "Economics Major, Mathematics Minor",
+	"dates": "2014-2017",
+	"url": "www.oxy.edu"
+};
+
+education.schools[1] = {
+	"name": "City College of San Francisco",
+	"location": "San Francisco, CA",
+	"degree": "none",
+	"majors": "Coursework in German, Bookbinding",
+	"dates": "2013-2014",
+	"url": "www.ccsf.edu"
+};
+
+education.schools[2] = {
+	"name": "Harvard University",
+	"location": "Cambridge, MA",
+	"degree": "Harvard College Class of 2015",
+	"majors": "Coursework in . Working toward B.A. but transferred to Occidental College",
+	"dates": "2011-2012",
+	"url": "www.harvard.edu"
+};
+
+education.onlineCourses" = [];
+
+education.onlineCourses[0] = {
+	"title": "Front-End Web Developer Nanodegree, Expected August 2016",
+	"school": "Udacity",
+	"dates": "June 2016-August 2016",
+	"url": "www.udacity.com"
+};
+
+//education.display = function taking no parameters
+
 */
