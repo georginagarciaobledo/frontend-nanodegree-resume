4540fb9e8971b1bb475fbfee32fd6d4463099086
Georgina Garcia-Obledo <georgina376@gmail.com>
2016-07-23 17:45:01 -0700

    use string.replace to replace awesome in awesomeThoughts with fun to create new var funThoughts and append funThoughts to main

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,6 @@
 $("#main").append("Georgina Garcia-Obledo");
 var awesomeThoughts = "I am Georgina, and I am AWESOME!";
 console.log(awesomeThoughts);
+var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
+console.log(funThoughts);
+$("#main").append(funThoughts);
