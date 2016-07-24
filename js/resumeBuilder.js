a031d04702d1d54d7b76a8be9884987495246864
Georgina Garcia-Obledo <georgina376@gmail.com>
2016-07-23 17:24:42 -0700

    test jquery append add name to main

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,8 +1,1 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
- var bio = {   "name" : "Georgina",   "role": "Web Developer" };
- var formattedRole = HTMLheaderRole.replace("%data%", bio.role);   
- $("#title").prepend(formattedRole);   
- var formattedName = HTMLheaderName.replace("%data%", bio.name);   
- $("#title").prepend(formattedName);
+$("#main").append("Georgina Garcia-Obledo");
