c5b114eb6df763d7d8ef3e5e938c01051d7640e8
Georgina Garcia-Obledo <georgina376@gmail.com>
2016-07-23 20:13:59 -0700

    create object bio place in scratch because when add to js/resumeBuilder.js everything disappears from page not sure why

--- js/scratch.js
+++ js/scratch.js
@@ -3,3 +3,19 @@
  $("#title").prepend(formattedRole);   
  var formattedName = HTMLheaderName.replace("%data%", bio.name);   
  $("#title").prepend(formattedName);
+
+
+ var bio = {
+	"name": "Georgina Garcia-Obledo",
+	"role": "Product Manager",
+	"contact info": {
+		"email": "georgina376@gmail.com",
+		"github": "georginagarciaobledo",
+		"location": "San Francisco/Los Angeles"
+	},
+	"welcomeMessage": "Welcome to my interactive resume.";
+	"skills": ["user research", "event planning"],
+	"bioPic": "images/headshot.jpg"
+};
+
+$("#main").append(bio.location);
