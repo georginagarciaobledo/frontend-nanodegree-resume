42a26060cffc40943c38506c1a33d1760d90ed84
Georgina Garcia-Obledo <georgina376@gmail.com>
2016-07-25 17:24:19 -0700

    refactor: reformat resumeBuilder.js so that matches one in video. Confused as to why there are no semicolons between the 4 main objects, but just letting it be for now...

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,131 +1,114 @@
- //Work Object Below:
-var work = {};
-
-work.jobs = [];
-
-work.jobs[0] = {
-	"employer": "Cocoon Cam",
-	"title": "Customer Success Intern",
-	"location": "San Francisco, CA",
-	"dates": "June 2016-Present",
-	"description": "This is what I do. Here is our website: www.cocooncam.com"
-};
-
-work.jobs[1] = {
-	"employer": "Occidental College",
-	"title": "Managing Editor, La Encina Yearbook",
-	"location": "San Francisco, CA",
-	"dates": "May 2016-Present",
-	"description": "This is what I do. Here is our website: www.oxy.edu/la-encina"
-};
-
-work.jobs[2] = {
-	"employer": "Occidental College",
-	"title": "Economics Students Association",
-	"location": "San Francisco, CA",
-	"dates": "May 2016-Present",
-	"description": "This is what I do. Here is our website: ..."
-};
-
-work.jobs[3] = {
-	"employer": "Koru@Facebook",
-	"title": "Program Participant",
-	"location": "San Francisco, CA",
-	"dates": "July 2015",
-	"description": "This is what I do. Here is our website: joinkoru.com"
-};
-
-work.jobs[4] = {
-	"employer": "Cullinane Construction",
-	"title": "Admin Assistant",
-	"location": "San Francisco, CA",
-	"dates": "December 2013-Present",
-	"description": "This is what I do. I worked here full time December 2013-July 2014. Now part-time."
-};
-
-//work.display = function taking no parameters
-
-//Projects Object Below:
-var projects = {}; 
-
-projects.projects = []; 
-
-projects.projects[0] = {
-	"title": "Portfolio Project",
-	"dates": "July 2016",
-	"description": "Made an online portfolio.", 
-	"images": ["picture1.jpg", "picture2.jpg"]
-};
-
-projects.projects[1] = {
-	"title": "Animal Trading Card",
-	"dates": "June 2016",
-	"description": "Made a trading card based on a picture.",
-	"images": ["pictures.jpg", "pictures2.jpg"]
-}
-//projects.display = function taking no parameters
-
-
  //Bio Object Below: 
  var bio = {
- 	"name" : "Georgina Garcia-Obledo",
- 	"role" : "Product Manager",
- 	"contacts" : {
- 		"mobile" : "415.555.5555",
- 		"email" : "georgina376@gmail.com",
- 		"github" : "georginagarciaobledo",
- 		"location" : "San Francisco and Los Angeles",
-
+ 	"name": "Georgina Garcia-Obledo",
+ 	"role": "Product Manager",
+ 	"contacts": {
+ 		"mobile": "415.555.5555",
+ 		"email": "georgina376@gmail.com",
+ 		"github": "georginagarciaobledo",
+ 		"location": "San Francisco and Los Angeles",
  	},
- 	"welcomeMessage" : "Welcome, catchy intro blah blah blah.",
- 	"skills" : ["UX research"," coordination"," data analysis"],
- 	"biopic" : "images/headshot.jpg",
- 	//"display" : "function taking no parameters"
- };
-
+ 	"welcomeMessage": "Welcome, catchy intro blah blah blah.",
+ 	"skills": [
+ 		"UX research"," coordination"," data analysis"
+ 	],
+ 	"bioPic": "images/headshot.jpg",
+ }
 
 
 //Education Object Below:
-var education = {};
+var education = {
+	"schools: [
+		{
+			"name": "Occidental College",
+			"location": "Los Angeles, CA",
+			"degree": "B.A., Expected May 21, 2017",
+			"majors": ["Economics Major, Mathematics Minor"]
+			"dates": "2014-2017",
+			"url": "www.oxy.edu"
+		},
+		{
+			"name": "City College of San Francisco",
+			"location": "San Francisco, CA",
+			"degree": "none",
+			"majors": ["Coursework in German, Bookbinding"],			
+			"dates": "2013-2014",
+			"url": "www.ccsf.edu"
+		},
+		{
+			"name": "Harvard University",
+			"location": "Cambridge, MA",
+			"degree": "Harvard College Class of 2015",
+			"majors": ["Coursework in...", "Working toward B.A. but transferred to Occidental College"],
+			"dates": "2011-2012",
+			"url": "www.harvard.edu"
+		}
+	],
+	"onlineCourses": [
+		{
+			"title": "Front-End Web Developer Nanodegree, Expected August 2016",
+			"school": "Udacity",
+			"dates": "June 2016-August 2016",
+			"url": "www.udacity.com"
+		}
+	]
+}
 
-education.schools = [];
+//Work Object Below:
+var work = {
+	"jobs": [
+		{
+			"employer": "Cocoon Cam",
+			"title": "Customer Success Intern",
+			"location": "San Francisco, CA",
+			"dates": "June 2016-Present",
+			"description": "This is what I do. Here is our website: www.cocooncam.com"
+		},
+		{
+			"employer": "Occidental College",
+			"title": "Managing Editor, La Encina Yearbook",
+			"location": "San Francisco, CA",
+			"dates": "May 2016-Present",
+			"description": "This is what I do. Here is our website: www.oxy.edu/la-encina"
+		},
+		{
+			"employer": "Occidental College",
+			"title": "Economics Students Association",
+			"location": "San Francisco, CA",
+			"dates": "May 2016-Present",
+			"description": "This is what I do. Here is our website: ..."
+		},
+		{
+			"employer": "Koru@Facebook",
+			"title": "Program Participant",
+			"location": "San Francisco, CA",
+			"dates": "July 2015",
+			"description": "This is what I do. Here is our website: joinkoru.com"
+		},
+		{
+			"employer": "Cullinane Construction",
+			"title": "Admin Assistant",
+			"location": "San Francisco, CA",
+			"dates": "December 2013-Present",
+			"description": "This is what I do. I worked here full time December 2013-July 2014. Now part-time."
+		}
+	]
+}	
 
-education.schools[0] = {
-	"name": "Occidental College",
-	"location": "Los Angeles, CA",
-	"degree": "B.A., Expected May 21, 2017",
-	"dates": "2014-2017",
-	"url": "www.oxy.edu",
-	"majors": ["Economics Major, Mathematics Minor"]
-};
-
-education.schools[1] = {
-	"name": "City College of San Francisco",
-	"location": "San Francisco, CA",
-	"degree": "none",
-	"dates": "2013-2014",
-	"url": "www.ccsf.edu",
-	"majors": ["Coursework in German, Bookbinding"]
-};
-
-education.schools[2] = {
-	"name": "Harvard University",
-	"location": "Cambridge, MA",
-	"degree": "Harvard College Class of 2015",
-	"dates": "2011-2012",
-	"url": "www.harvard.edu",
-	"majors": ["Coursework in...", "Working toward B.A. but transferred to Occidental College"]
-};
-
-education.onlineCourses" = [];
-
-education.onlineCourses[0] = {
-	"title": "Front-End Web Developer Nanodegree, Expected August 2016",
-	"school": "Udacity",
-	"dates": "June 2016-August 2016",
-	"url": "www.udacity.com"
-};
-
-//education.display = function taking no parameters
-
-
+//Projects Object Below:
+var projects = {
+	"projects": [
+		{
+			"title": "Portfolio Project",
+			"dates": "July 2016",
+			"description": "Made an online portfolio.", 
+			"images": ["picture1.jpg", "picture2.jpg"]
+		},
+		{
+			"title": "Animal Trading Card",
+			"dates": "June 2016",
+			"description": "Made a trading card based on a picture.",
+			"images": ["pictures.jpg", "pictures2.jpg"]
+		},
+	]
+}
